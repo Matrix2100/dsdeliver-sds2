679e011934b4e4865e534c1959ab87950f58c8b0
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-01-08 19:59:23 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <>
+        <Navbar />
+        <Home />
+      </>
     </div>
   );
 }
