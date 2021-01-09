d948d9f3bfa25b57c88acb67be2f7f9acdda17a5
Vinicius Oliveira Gouveia <vinicius.oliveira00@souunit.com.br>
2021-01-08 22:55:48 -0300

    Add cards and products backend working

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import './styles.css';
-import { ReactComponent as Logo } from './logo.svg';
 import StepsHeader from "./stepsHeader";
+import ProductsList from "./productsList";
+import { useState } from "react";
+import { Product } from "./types";
+import { fetchProducts } from "../api";
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products)
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
     )
 }
